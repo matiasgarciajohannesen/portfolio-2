5f3f1cad66781e94518d43bd8395118ea5888d88
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2022-01-24 06:09:12 +0100

    flyttet filer
    flyttet filer

---
+++ portfolio/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="logo">Matias G. Johannesen</h1>
+        <button>Contact</button>
+      </header>
+      
+      <body>
+          <div className="top"> 
+            <h2 className="front">Front-End</h2>
+              <h2 className="dev">Developer</h2>
+            <div className="info">
+              <h3>About me:</h3>
+              <p>Im a 23 year old Front-end developer based in Bergen, Norway</p>
+            </div>
+          </div>
+
+          <div className="links">
+            <div className="link-1"></div>
+            <div className="link-2"></div>
+            <div className="link-3"></div>
+            <div className="link-4"></div>
+          </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
